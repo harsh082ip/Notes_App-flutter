f1769d99041493a4f85a1c97ff21976148fc3ee1
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-08-19 14:37:31 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/harsh082ip/notes_app/notes_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.harsh082ip.notes_app.notes_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/screen/auth/login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.amber,
+      ),
+    );
+  }
+}
